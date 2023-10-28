e005d16e294030d88b03dde757a7922de6b4320a
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2023-10-28 11:30:31 +0530

    Ctrl Slash works now but on ctrl slashing(i.e changing if it has checkbox or not it loses focus

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,48 @@ import "./App.css"
 function App() {
 
   const [data, setData] = useState([
-    { category: { isCheckBox: false, isChecked: false } , data: "first\nsecond"},
-    { category: { isCheckBox: true, isChecked: false } , data: "This is a checkbox" },
-    { category: { isCheckBox: true, isChecked: true } , data: "This is the second checkbox" },
-    { category: { isCheckBox: false, isChecked: false } , data: "First line\nSecond line"}
+    { category: { isCheckBox: false, isChecked: false }, data: "first\nsecond" },
+    { category: { isCheckBox: true, isChecked: false }, data: "This is a checkbox" },
+    { category: { isCheckBox: true, isChecked: true }, data: "This is the second checkbox" },
+    { category: { isCheckBox: false, isChecked: false }, data: "First line\nSecond line" }
   ])
 
   const wasAddButtonClicked = useRef(false)
+  const indexOfElementCtrlSlashed = useRef(false)
 
-  function handleKeyDown(e,index) {
+  function handleCtrlEnter(e, index) {
+    const preText = data[index].data.slice(0, e.target.selectionStart)
+    const postText = data[index].data.slice(e.target.selectionStart)
+    createNewElement(index + 1, postText, preText)
+    console.log("Ctrl + Enter was pressed!!")
+  }
+
+  function handleCtrlSlash (index) {
+    const duplicate = JSON.parse(JSON.stringify(data))
+    duplicate[index].category.isCheckBox = !(duplicate[index].category.isCheckBox)
+    setData(duplicate)
+  }
+
+  function handleKeyDown(e, index) {
     if (e.key === "Enter" && e.ctrlKey) {
-      const preText = data[index].data.slice(0,e.target.selectionStart)
-      const postText = data[index].data.slice(e.target.selectionStart)
-      createNewElement(index+1, postText, preText)
-      console.log("Ctrl + Enter was pressed!!")
+      handleCtrlEnter(e,index)
+    }
+    else if(e.key === "/" && e.ctrlKey) {
+      console.log("Ctrl + / was pressed")
+      indexOfElementCtrlSlashed.current = index
+      handleCtrlSlash(index)
     }
   }
 
-  function createNewElement(index = -1,value = "", preText = "") {
+  function createNewElement(index = -1, value = "", preText = "") {
     const duplicate = JSON.parse(JSON.stringify(data))
-    
-    if(index !== -1) {
-      duplicate[index-1].data = preText
+
+    if (index !== -1) {
+      duplicate[index - 1].data = preText
     }
 
-    const elementArr = { category: { isCheckBox: false, isChecked: false } , data: value}
-    duplicate.splice(index,0,elementArr)
+    const elementArr = { category: { isCheckBox: false, isChecked: false }, data: value }
+    duplicate.splice(index, 0, elementArr)
     setData(duplicate)
   }
 
@@ -58,13 +74,13 @@ function App() {
 
   function handleButtonClick() {
     const duplicate = JSON.parse(JSON.stringify(data))
-    duplicate.push({ category: { isCheckBox: false, isChecked: false } , data: ""})
+    duplicate.push({ category: { isCheckBox: false, isChecked: false }, data: "" })
     wasAddButtonClicked.current = true
     setData(duplicate)
   }
 
-  function callbackForRef(node,index) {
-    if (index === data.length-1 && node && wasAddButtonClicked.current) {
+  function callbackForRef(node, index) {
+    if (index === data.length - 1 && node && wasAddButtonClicked.current) {
       node.focus()
       wasAddButtonClicked.current = false
     }
@@ -72,17 +88,17 @@ function App() {
 
   function deleteElement(index) {
     const duplicate = JSON.parse(JSON.stringify(data))
-    duplicate.splice(index,1)
+    duplicate.splice(index, 1)
     setData(duplicate)
   }
 
   useLayoutEffect(() => {
     const collection = document.getElementsByClassName("textarea")
-    for (let i = 0; i < collection.length ; i+=1) {
+    for (let i = 0; i < collection.length; i += 1) {
       collection[i].style.height = "0px"
-      collection[i].style.height = `${(collection[i].scrollHeight)+4}px`
+      collection[i].style.height = `${(collection[i].scrollHeight) + 4}px`
     }
-  },[data])
+  }, [data])
 
   return (
     <div id="individualNoteContainer" >
@@ -91,14 +107,14 @@ function App() {
           return (
             <div className="textAreaContainer" key={index} style={{ display: "flex", justifyContent: "flex-start", alignItems: "flex-start" }} >
               <input type="checkbox" checked={data[index].category.isChecked} onChange={() => handleCheckChange(index)} />
-              <textarea style={getStyles(index)} value={data[index].data} className="textarea" onChange={(e) => handleTextChange(e,index)} onKeyDown={(e) => handleKeyDown(e,index)} />
+              <textarea style={getStyles(index)} value={data[index].data} className="textarea" onChange={(e) => handleTextChange(e, index)} onKeyDown={(e) => handleKeyDown(e, index)} />
               <button className="deleteButton" onClick={() => deleteElement(index)} >Del</button>
             </div>
           )
         }
         return (
-          <div className="textAreaContainer" key={index} style={{display : "flex", alignItems : "flex-start", justifyContent : "space-between" }} >
-            <textarea style={{resize : "none",display : "block"}}  value={data[index].data} className="textarea" ref={(node) => callbackForRef(node,index)} onChange={(e) => handleTextChange(e,index)} onKeyDown={(e) => handleKeyDown(e,index)} />
+          <div className="textAreaContainer" key={index} style={{ display: "flex", alignItems: "flex-start", justifyContent: "space-between" }} >
+            <textarea style={{ resize: "none", display: "block" }} value={data[index].data} className="textarea" ref={(node) => callbackForRef(node, index)} onChange={(e) => handleTextChange(e, index)} onKeyDown={(e) => handleKeyDown(e, index)} />
             <button className="deleteButton" onClick={() => deleteElement(index)} >Del</button>
           </div>
         )
