5bde2331c1effbd0a918de372ca6152d57204963
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2024-05-01 23:47:06 +0530

    works

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@
 // import useThrottle from "./customHooks/useThrottle"
 // import { TooltipButtonWrapper } from "./components/tooltipButton"
 // import { OpenNote } from "./components/OpenNote"
+import { NoteInHomePage } from "./components/NoteInHomePage"
 function App() {
-  
-  // return <OpenNote />
+  return <NoteInHomePage />
 }
 
 export default App
---
+++ src/components/NoteInHomePage.jsx
@@ -0,0 +1,30 @@
+import "./NoteInHomePage.css"
+
+function NoteInHomePage() {
+  const data = "first\nsecond\nthird\nfourth\nfifth"
+
+  function getStringWithReducedInfo(str) {
+    if(str.length > 84) {
+      return str.slice(0,)
+    }
+
+
+
+    const arrOfLines = str.split("\n")
+    console.log(arrOfLines)
+    if(arrOfLines.length > 3) {
+      return (arrOfLines.slice(0,3).join("\n")+"\n...")
+    }
+
+  }
+  
+  return (
+    <>
+      <textarea value={getStringWithReducedInfo(data)} style={{height : `${28*1.1*4}px`}} />
+    </>
+  )
+}
+
+export {
+  NoteInHomePage
+}
