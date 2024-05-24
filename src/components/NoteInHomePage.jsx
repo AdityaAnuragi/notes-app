bf26f9214389c24ba7d8d42d1ce52928461fdb63
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2024-05-24 15:17:57 +0530

    homepage checkbox and Text pair component is now functional

--- src/components/NoteInHomePage.jsx
+++ src/components/NoteInHomePage.jsx
@@ -4,14 +4,21 @@ import { Text } from "@mantine/core"
 
 const placeholderText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam reiciendis ipsa est, aliquid illo iste esse qui eum dicta?"
 
-function NoteInHomePage({ lineClamp = 3, size = "xl", value = placeholderText }) {
+function NoteInHomePage({ lineClamp = 3, size = "xl", value = placeholderText , checkboxData = {hasCheckbox : true , isTicked : true} }) {
+  
   return (
     <>
       <div className={`${note.textContainer}`} >
 
-        <input type="checkbox" className={note.myCheckbox} />
+        {checkboxData.hasCheckbox && <input type="checkbox" checked={checkboxData.isTicked} className={note.myCheckbox} readOnly />}
 
-        <Text lineClamp={lineClamp} size={size} >
+        <Text 
+          lineClamp={lineClamp} 
+          size={size} 
+          classNames={{
+            root : checkboxData.isTicked ? note.mantineTextRootSelector : null
+          }}
+        >
           {value}
         </Text>
       </div>
