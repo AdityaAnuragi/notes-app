40c3b86166282a3d5cc0c97a6b2b989e42178553
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2024-05-24 19:19:13 +0530

    placed the open note on top

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@
 // import "./App.css"
 // import useThrottle from "./customHooks/useThrottle"
 // import { TooltipButtonWrapper } from "./components/tooltipButton"
-// import { OpenNote } from "./components/OpenNote"
+import { OpenNote } from "./components/OpenNote"
 import { CheckboxAndTextPairInHomePageNote } from "./components/CheckboxAndTextPairInHomePageNote"
 function App() {
   return (
     <>
-      {/* <OpenNote /> */}
+      {false && <OpenNote />}
       <CheckboxAndTextPairInHomePageNote />
-      <CheckboxAndTextPairInHomePageNote value="hey there" checkboxData={{hasCheckbox : true , isTicked : false}} />
+      {/* <CheckboxAndTextPairInHomePageNote value="hey there" checkboxData={{hasCheckbox : true , isTicked : false}} />
       <CheckboxAndTextPairInHomePageNote value="hey there" checkboxData={{hasCheckbox : false , isTicked : false}} />
-      <CheckboxAndTextPairInHomePageNote value="qwertyuiopasdfghjkklzxcvbnmqwertyuiop" />
+      <CheckboxAndTextPairInHomePageNote value="qwertyuiopasdfghjkklzxcvbnmqwertyuiop" /> */}
     </>
   )
 }
