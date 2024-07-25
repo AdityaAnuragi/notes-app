be6420a07d443f696a921e831b1aded3ec6fc9d2
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2024-07-25 19:32:19 +0530

    new note button added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,22 @@ import { NoteInHomePage } from "./components/NoteInHomePage"
 import styles from "./App.module.css"
 import { useEffect, useLayoutEffect, useRef, useState } from "react"
 function App() {
-  const [n , setN] = useState(1)
+
+  const [allNotes , setAllNotes] = useState([
+    [
+      { category: { isCheckBox: false, isChecked: false }, data: "Sample data" }
+    ],
+    [
+      { category: { isCheckBox: false, isChecked: false }, data: "Hold ctrl to see shortcut" },
+      { category: { isCheckBox: true, isChecked: true }, data: "this is ticked" },
+      { category: { isCheckBox: false, isChecked: false }, data: "regular note" }
+    ],
+    [
+      { category: { isCheckBox: false, isChecked: false }, data: "second note" }
+    ]
+  ])
+
+  const [n, setN] = useState(1)
   // console.log(n)
   const containerRef = useRef()
 
@@ -10,11 +25,11 @@ function App() {
     // console.log(document.getElementsByTagName("html")[0].clientWidth)
     const windowWidth = document.getElementsByTagName("html")[0].clientWidth
     // console.log(`inside, ${windowWidth}`)
-    if(windowWidth < 425) {
+    if (windowWidth < 425) {
       return 1
     }
 
-    else if(windowWidth < 1024) {
+    else if (windowWidth < 1024) {
       return 2
     }
 
@@ -22,6 +37,16 @@ function App() {
       return 3
     }
 
+  }
+
+  function addNewNote() {
+    setAllNotes(prev => {
+      const duplicate = JSON.parse(JSON.stringify(prev))
+      duplicate.unshift([
+        { category: { isCheckBox: false, isChecked: false }, data: "A fresh new note" }
+      ])
+      return duplicate
+    })
   }
 
   useLayoutEffect(() => {
@@ -35,24 +60,24 @@ function App() {
 
     const totalColumns = totalColumnsOnScreen()
     const arrOfNotesOnly = []
-    for( const ele of  containerRef.current.children) {
+    for (const ele of containerRef.current.children) {
       if (!(ele.className.includes(styles.fillSpace))) {
         arrOfNotesOnly.push(ele)
       }
     }
-    
+
     let maxHeightOfAllColumns = 0
     let i = 0
-    while(i < totalColumns ) {
+    while (i < totalColumns) {
       let j = i
       let heightOfColumn = 0
-      while(j < arrOfNotesOnly.length) {
+      while (j < arrOfNotesOnly.length) {
         // console.log(arrOfNotesOnly[j] , arrOfNotesOnly[j].clientHeight)
-        heightOfColumn  += arrOfNotesOnly[j].clientHeight
-        j+=totalColumns
+        heightOfColumn += arrOfNotesOnly[j].clientHeight
+        j += totalColumns
       }
       maxHeightOfAllColumns = heightOfColumn > maxHeightOfAllColumns ? heightOfColumn : maxHeightOfAllColumns
-      i+=1
+      i += 1
     }
 
     // console.log(`Max ht required is ${maxHeightOfAllColumns}`)
@@ -61,22 +86,22 @@ function App() {
     // console.log(`cols : ${totalColumns}`)
     // console.log("")
 
-    containerRef.current.style.height = `${maxHeightOfAllColumns+60}px`
+    containerRef.current.style.height = `${maxHeightOfAllColumns + 60}px`
   })
 
   useEffect(() => {
-    window.addEventListener("resize",() => setN((n) => n+1))
-  },[])
+    window.addEventListener("resize", () => setN((n) => n+1))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <>
+      <button className={styles.newNoteButton} onClick={addNewNote} >New Note</button>
       <div className={styles.container} ref={containerRef} >
-        <NoteInHomePage elementsInHomeNote={1} />
-        <NoteInHomePage elementsInHomeNote={2} />
-        <NoteInHomePage elementsInHomeNote={3} />
-        <NoteInHomePage elementsInHomeNote={4} />
-        <NoteInHomePage elementsInHomeNote={5} />
-        <NoteInHomePage elementsInHomeNote={6} />
+        
+        {allNotes && allNotes.map((note,index) => {
+          return <NoteInHomePage key={index} completeHomePageNoteData={note} />
+        })}
 
         <div className={styles.fillSpace} ></div>
         <div className={styles.fillSpace} ></div>
--- src/components/NoteInHomePage.jsx
+++ src/components/NoteInHomePage.jsx
@@ -1,11 +1,21 @@
 import { CheckboxAndTextPairInHomePageNote } from "./CheckboxAndTextPairInHomePageNote";
 import styles from "./NoteInHomePage.module.css"
 
-export function NoteInHomePage({elementsInHomeNote = 3}) {
+export function NoteInHomePage({ completeHomePageNoteData }) {
   const arr = []
 
-  for(let i = 1 ; i <= elementsInHomeNote ; i++) {
-    arr.push(<CheckboxAndTextPairInHomePageNote key={i} />)
+  // for(let i = 1 ; i <= elementsInHomeNote ; i++) {
+  //   arr.push(<CheckboxAndTextPairInHomePageNote key={i} />)
+  // }
+
+  // [
+  //   { category: { isCheckBox: false, isChecked: false }, data: "Hold ctrl to see shortcut" },
+  //   { category: { isCheckBox: true, isChecked: true }, data: "this is ticked" },
+  //   { category: { isCheckBox: false, isChecked: false }, data: "regular note" }
+  // ]
+
+  for( const note of completeHomePageNoteData ) {
+    arr.push( <CheckboxAndTextPairInHomePageNote value={note.data} checkboxData={{hasCheckbox : note.category.isCheckBox,isTicked : note.category.isChecked}} /> )
   }
 
   return (
