ae1f0eb3df2b014d8465b58a256a987f1b14e0a0
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2024-07-29 18:38:19 +0530

    cursor set to pointer for the notes in homepage

--- src/components/NoteInHomePage.jsx
+++ src/components/NoteInHomePage.jsx
@@ -2,25 +2,6 @@ import { CheckboxAndTextPairInHomePageNote } from "./CheckboxAndTextPairInHomePa
 import styles from "./NoteInHomePage.module.css"
 
 export function NoteInHomePage({ completeHomePageNoteData }) {
-    // for(let i = 1 ; i <= elementsInHomeNote ; i++) {
-  //   arr.push(<CheckboxAndTextPairInHomePageNote key={i} />)
-  // }
-
-  // [
-  //   { category: { isCheckBox: false, isChecked: false }, data: "Hold ctrl to see shortcut" },
-  //   { category: { isCheckBox: true, isChecked: true }, data: "this is ticked" },
-  //   { category: { isCheckBox: false, isChecked: false }, data: "regular note" },
-  //   { category: { isCheckBox: false, isChecked: false }, data: "A fourth item that is hidden" }
-  // ]
-  
-  // const arr = completeHomePageNoteData.map((note,index) => {
-  //   const {data,category} = note
-  //   return <CheckboxAndTextPairInHomePageNote key={index} value={data} checkboxData={{hasCheckbox : category.isCheckBox, isTicked : category.isChecked}} />
-  // })
-
-  // console.log(completeHomePageNoteData)
-
-  // console.log(loopUpperLimit)
   
   const loopUpperLimit = (completeHomePageNoteData.length > 3) ? 3 : completeHomePageNoteData.length
   const arr = []
